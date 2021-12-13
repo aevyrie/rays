06909225d9df0f92e283bf08e32c5979fb73afcc
Aevyrie <aevyrie@gmail.com>
2021-12-13 00:31:56 -0800

    Workspace setup

---
+++ rays_gui/src/app.rs
@@ -0,0 +1,77 @@
+use eframe::{
+    egui::{self, TextureId},
+    epi,
+};
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
+    // this how you opt-out of serialization of a member
+    //#[cfg_attr(feature = "persistence", serde(skip))]
+    texture_id: TextureId,
+    render_size: [f32; 2],
+}
+
+impl Default for TemplateApp {
+    fn default() -> Self {
+        Self {
+            texture_id: TextureId::User(0),
+            render_size: [500.0, 500.0],
+        }
+    }
+}
+
+impl epi::App for TemplateApp {
+    fn name(&self) -> &str {
+        "Rays"
+    }
+
+    /// Called once before the first frame.
+    fn setup(
+        &mut self,
+        _ctx: &egui::CtxRef,
+        _frame: &mut epi::Frame<'_>,
+        _storage: Option<&dyn epi::Storage>,
+    ) {
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        #[cfg(feature = "persistence")]
+        if let Some(storage) = _storage {
+            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
+        }
+    }
+
+    /// Called by the frame work to save state before shutdown.
+    /// Note that you must enable the `persistence` feature for this to work.
+    #[cfg(feature = "persistence")]
+    fn save(&mut self, storage: &mut dyn epi::Storage) {
+        epi::set_value(storage, epi::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        let Self {
+            texture_id,
+            render_size,
+        } = self;
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        frame.quit();
+                    }
+                });
+            });
+        });
+
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("Side Panel");
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| ui.image(*texture_id, *render_size));
+    }
+}
